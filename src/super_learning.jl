1c2910ede74d1cb9a4422b3cdfd9042e788c9d6c
Olivier Labayle <olabayle@gmail.com>
2021-05-04 19:35:00 +0100

    work in progress superlearner

--- src/super_learning.jl
+++ src/super_learning.jl
@@ -1,35 +1,106 @@
-using MLJ, MLJBase, DataFrames
+using MLJ, MLJBase
+using MLJLinearModels: LogisticClassifier
+using DataFrames
 
 
 """
 Implementation of the SuperLearning routine
 """
-function superlearn(library, X, y; nfolds=10)
-    stratified_cv = StratifiedCV(; nfolds=nfolds, shuffle=false)
-    nfolds_iter = MLJBase.train_test_pairs(stratified_cv, 1:length(y), y)
+function superlearn(library, X, y; nfolds=10, refit=false)
+    # Define the learning network
+    Xs = source(X)
+    ys = source(y)
+    Z = DataFrame([])
+    for model in library
+        m = machine(model, Xs, ys)
+        Z_val_fold[!, name(model)] = predict(m, Xs)
+    end
 
-    # Y_val and y_val define a new training set that 
-    # will be used to fit the SuperLearner coefficients with :
-    # Y_val[i, model_name] = predict(model, X_val[i, :])
-    Y_val = DataFrame([])
-    y_val = categorical([])
+    y_hat = nothing
+
+    n, = size(y)
+    stratified_cv = StratifiedCV(; nfolds=nfolds, shuffle=false)
+    nfolds_iter = MLJBase.train_test_pairs(stratified_cv, 1:n, y)
+
+
+    Z_val = DataFrame([])
+    # Unfortunately can't seem to instantiate empty categorical array
+    y_val = nothing
 
     # Loop over the different folds
     for (train, val) in nfolds_iter
         X_val = X[val, :]
-        Y_val_fold = DataFrame([])
+        Z_val_fold = DataFrame([])
 
         # Loop over the different models in the library
         for model in library
             m = machine(model, X, y)
             fit!(m, rows=train)
-            Y_val_fold[!, name(model)] = predict(m, X_val)
+            Z_val_fold[!, name(model)] = predict(m, X_val)
         end
 
+        Z_val = vcat(Z_val, Z_val_fold)
+
+        if y_val === nothing
+            y_val = y[val]
+        else
+            y_val = vcat(y_val, y[val])
+        end
         
-        Y_val = vcat(Y_val, Y_val_fold)
-        y_val = vcat(y_val, y[val])
     end
 
-    
+    metalearner = machine(LogisticClassifier(), Z_val, y_val)
+    fit!(metalearner, rows=1:n)
+
+    if refit
+        for model in library
+            m = machine(model, X, y)
+            fit!(m)
+        end
+    end
+
+    metalearner
 end
+
+
+mutable struct SuperLearner <: DeterministicNetwork
+    library
+    metalearner
+end
+
+
+knc = @load DecisionTreeClassifier pkg=DecisionTree
+lr = LogisticClassifier()
+
+library = [knc(), lr]
+
+function MLJ.fit(m::SuperLearner, verbosity::Int, X, y)
+    X = source(X)
+    y = source(y)
+
+    Z = nothing
+    for model in m.library
+        mach = machine(model, X, y)
+
+        if Z === nothing
+            Z = predict(mach, X)
+        else
+            hcat(Z, predict(mach, X))
+        end
+    end
+
+    mach = machine(m.metalearner, Z, y)
+    ŷ = predict(mach, Z)
+
+    mach = machine(Deterministic(), X, y; predict=ŷ)
+    fit!(mach, verbosity=verbosity - 1)
+    return mach()
+
+end
+
+
+
+
+
+
+sl = superlearner(library)
--- tests/super_learning.jl
+++ tests/super_learning.jl
@@ -1,17 +1,24 @@
 using GenesInteraction
-using MLJ, Test, DataFrames, CategoricalArrays, MLJLinearModels
+using Distributions, MLJ, Test, DataFrames, CategoricalArrays, MLJLinearModels
 
+function simulation_dataset()
+    n = 1000
+    Age = rand(DiscreteUniform(30, 70), n)
+    X_1 = 1 ./ (1 .+ exp.(-0.005*Age .- 0.01)) .< rand(Uniform(0,1), n)
+    X_2 = 1 ./ (1 .+ exp.(-0.01*Age)) .< rand(Uniform(0,1), n)
+    X = DataFrame(
+        locus_1 = categorical(X_1),
+        locus_2 = categorical(X_2),
+        age = Age
+    )
 
-X = DataFrame(
-    locus_1 = categorical(['A', 'C', 'C', 'G', 'A', 'C']),
-    locus_2 = categorical(['C', 'T', 'T', 'T', 'C', 'C']),
-    age = [45., 56., 34., 65., 24., 54.]
-)
-
-y = categorical([0, 0, 0, 1, 1, 0])
+    y = 1 ./ (1 .+ exp.(-0.01*Age - 0.94*X_1 + 0.4*X_2)) .< rand(Uniform(0,1), n)
+    X, categorical(y)
+end
 
 library = [@pipeline(OneHotEncoder(), 
                     LogisticClassifier(), 
                     name="MyPipeline")]
 
-GenesInteraction.superlearn(library, X, y;nfolds=2)
+X, y = simulation_dataset()
+metalearner = GenesInteraction.superlearn(library, X, y;nfolds=3, refit=false)
