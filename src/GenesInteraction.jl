487190105edc3d10993624f832eb6ed572e132a7
Olivier Labayle <48188914+olivierlabayle@users.noreply.github.com>
2021-08-27 15:16:57 +0100

    Interaction (#45)
    * organize repo
    
    * change and move combinetotable
    
    * add first canvas interaction ate
    
    * adds interaction ATE first implementation
    
    * remove some boilerplate
    
    * add continuous target test
    
    * add simulations ate
    
    * some more simulations
    
    * remove hot encoding from interactionate
    
    * remove hot encoding from ate and abstract covariate
    
    * extract tmle from the repo and start anew
    
    * add example data
    
    * build tmle from toml file
    
    * add test tmle built from config
    
    * first draft of workflow, needs some tests
    
    * adds some tests and remove some deps
    
    * add first version
    
    * try add manifest to solve CI issue

--- src/GenesInteraction.jl
+++ src/GenesInteraction.jl
@@ -1,27 +1,181 @@
 module GenesInteraction
 
-using Distributions: expectation
-using Tables
-using Distributions
-using CategoricalArrays
+using DataFrames
+using CSV
+using SnpArrays
 using MLJ
-using GLM
+using Distributions
+using TMLE
+using TOML
 
-import MLJ.fit
-import GLM.checky
-import GLM.devresid
+###############################################################################
+# REGRESSORS
 
-# #############################################################################
+RandomForestRegressor = @load RandomForestRegressor pkg=DecisionTree verbosity = 0
+NuSVR = @load NuSVR pkg=LIBSVM verbosity = 0
+LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity = 0
+KNNRegressor = @load KNNRegressor pkg=NearestNeighborModels verbosity = 0
+NeuralNetworkRegressor = @load NeuralNetworkRegressor pkg=MLJFlux verbosity = 0
+EvoTreeRegressor = @load EvoTreeRegressor pkg=EvoTrees verbosity = 0
+
+###############################################################################
+# CLASSIFIERS
+
+RandomForestClassifier= @load RandomForestClassifier pkg=DecisionTree verbosity = 0
+NuSVC = @load NuSVC pkg=LIBSVM verbosity = 0
+LogisticClassifier = @load LogisticClassifier pkg=MLJLinearModels verbosity = 0
+KNNClassifier = @load KNNClassifier pkg=NearestNeighborModels verbosity = 0
+NeuralNetworkClassifier = @load NeuralNetworkClassifier pkg=MLJFlux verbosity = 0
+EvoTreeClassifier = @load EvoTreeClassifier pkg=EvoTrees verbosity = 0
+
+###############################################################################
+# BUILD TMLE FROM .TOML
+
+
+function stack_from_config(config)
+    # Define the metalearner
+    metalearner = config["outcome"]["type"] == "categorical" ? 
+        LogisticClassifier(fit_intercept=false) : LinearRegressor(fit_intercept=false)
+
+    # Define the resampling strategy
+    resampling = config["resampling"]
+    resampling = eval(Symbol(resampling["type"]))(nfolds=resampling["nfolds"])
+
+    # Define the models library
+    models = Dict()
+    for (modelname, hyperparams) in config
+        if !(modelname in ("resampling", "outcome"))
+            modeltype = eval(Symbol(modelname))
+            paramnames = Tuple(Symbol(x[1]) for x in hyperparams)
+            counter = 1
+            for paramvals in Base.Iterators.product(values(hyperparams)...)
+                model = modeltype(;NamedTuple{paramnames}(paramvals)...)
+                models[Symbol(modelname*"_$counter")] = model
+                counter += 1
+            end
+        end
+    end
+
+    # Define the Stack
+    Stack(;metalearner=metalearner, resampling=resampling, models...)
+end
+
+
+function tmle_from_toml(config::Dict)
+
+    ytype = config["Q"]["outcome"]["type"]
+    distr = nothing
+    if ytype == "categorical"
+        distr = Bernoulli()
+    elseif ytype == "continuous"
+        distr = Normal()
+    else
+        error("The type of y should be either continuous or categorical")
+    end
+
+    Qstack = stack_from_config(config["Q"])
+    Gstack = stack_from_config(config["G"])
+
+    return InteractionATEEstimator(Qstack, Gstack, distr)
+
+end
+
+
+###############################################################################
+# RUN EPISTASIS ESTIMATION
+
+"""
+    filtering_idx(snparray, snpquery, snp_idx)
+Returns a list of indices for which people in snparray have a combination 
+of alleles given by the snpquery.
+"""
+function filtering_idx(snparray, snpquery, snp_idx)
+    interaction_order = size(snpquery)[1] ÷ 3
+    allindices = BitSet[]
+    columns = Int64[]
+    for i in 1:interaction_order
+        basecol_id = 3i - 2
+        snpcol = snp_idx[snpquery[basecol_id]]
+        snpindices = findall(
+            x -> (x == snpquery[basecol_id + 1] || x == snpquery[basecol_id + 2]), 
+            @view(snparray[:, snpcol])
+            )
+        push!(allindices, BitSet(snpindices))
+        push!(columns, snpcol)
+    end
+    
+    return collect(intersect(allindices...)), columns
+end
+
+"""
+
+Converts the treatment variables to categorical Table
+"""
+function prepareTreatment(T, snpquery::DataFrameRow)
+    interaction_order = size(snpquery)[1] ÷ 3
+    treatment_alleles = [snpquery[3i-1] for i in 1:interaction_order]
+    return MLJ.table(T .== transpose(treatment_alleles))
+end
+
+
+function prepareTarget(y, config)
+    if config["Q"]["outcome"]["type"] == "categorical"
+        y = categorical(y)
+    end
+    return y
+end
+
+
+"""
+TODO
+"""
+function tmleepistasis(genotypefile, 
+    phenotypefile, 
+    confoundersfile, 
+    snpfile, 
+    estimatorfile,
+    outfile;
+    verbosity=0)
+
+    config = TOML.parsefile(estimatorfile)
+    # Build tmle
+    tmle = tmle_from_toml(config)
+    # Read SNP data
+    snpdata = SnpData(genotypefile)
+    snp_idx = Dict((x => i) for (i,x) in enumerate(snpdata.snp_info.snpid))
+    # Read SNP Queries
+    snpqueries = CSV.File(snpfile) |> DataFrame
+    # Read Confounders
+    W = CSV.File(confoundersfile) |> DataFrame
+    # Read Target
+    y =  DataFrame(CSV.File(phenotypefile;header=false))[:, 3]
+    # Run TMLE over potential epistatic SNPS
+    estimates = []
+    stderrors = []
+    for snpquery in eachrow(snpqueries)
+        # Filter and prepare the data to retrieve only individuals 
+        # corresponding to the queried SNPs alleles and match `fit`
+        # expected input types
+        indices, columns = filtering_idx(snpdata.snparray, snpquery, snp_idx)
+        Wsnp = W[indices, :]
+        ysnp = prepareTarget(y[indices], config)
+        Tsnp = prepareTreatment(snpdata.snparray[indices, columns], snpquery)
+        # Estimate epistasis
+        # TODO: replace with machine syntax
+        fitresult, _, _ = TMLE.fit(tmle, verbosity, Tsnp, Wsnp, ysnp)
+        push!(estimates, fitresult.estimate)
+        push!(stderrors, fitresult.stderror)
+    end
+    # Save the results
+    snpqueries[!, "Estimate"] = estimates
+    snpqueries[!, "Standard Error"] = stderrors
+    CSV.write(outfile, snpqueries)
+end
+
+###############################################################################
 # EXPORTS
-# #############################################################################
 
-export ATEEstimator, fit
-
-# #############################################################################
-# INCLUDES
-# #############################################################################
-
-include("tmle.jl")
+export tmleepistasis
 
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,161 @@
-include("tmle.jl")
+using Test
+using GenesInteraction
+using MLJ
+using Distributions
+using TOML
+using CSV
+using DataFrames
+using CategoricalArrays
+
+genotypefile = joinpath("data", "mouse")
+phenotypefile = joinpath("data", "pheno_10_causals.txt")
+confoundersfile = joinpath("data", "confouders.csv")
+snpfile = joinpath("data", "query_snps.csv")
+
+LogisticClassifier = @load LogisticClassifier pkg=MLJLinearModels verbosity = 0
+KNNClassifier = @load KNNClassifier pkg=NearestNeighborModels verbosity = 0
+LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity = 0
+KNNRegressor = @load KNNRegressor pkg=NearestNeighborModels verbosity = 0
+
+
+@testset "Categorical target TMLE built from configuration file" begin
+    tmle_config = joinpath("config", "categorical.toml")
+    tmle =  GenesInteraction.tmle_from_toml(TOML.parsefile(tmle_config))
+    @test tmle.fluctuation_family isa Bernoulli
+
+    # Checking Qstack
+    Qstack = tmle.target_cond_expectation_estimator
+    ## Checking Qstack.metalearner
+    @test Qstack.metalearner isa LogisticClassifier
+    @test Qstack.metalearner.fit_intercept == false
+    ## Checking Qstack.resampling
+    @test Qstack.resampling isa StratifiedCV
+    @test Qstack.resampling.nfolds == 3
+    ## Checking Qstack KNN models
+    @test Qstack.KNNClassifier_1.K == 3
+    @test Qstack.KNNClassifier_2.K == 5
+    @test Qstack.KNNClassifier_3.K == 10
+    ## Checking Qstack Logistic models
+    @test Qstack.LogisticClassifier_1.lambda == 1.0
+    @test Qstack.LogisticClassifier_1.fit_intercept == true
+    @test Qstack.LogisticClassifier_2.lambda == 1.0
+    @test Qstack.LogisticClassifier_2.fit_intercept == false
+    @test Qstack.LogisticClassifier_3.lambda == 10
+    @test Qstack.LogisticClassifier_3.fit_intercept == true
+    @test Qstack.LogisticClassifier_4.lambda == 10
+    @test Qstack.LogisticClassifier_4.fit_intercept == false
+
+    # Checking Gstack
+    Gstack = tmle.treatment_cond_likelihood_estimator
+    ## Checking Gstack.metalearner
+    @test Gstack.metalearner isa LogisticClassifier
+    @test Gstack.metalearner.fit_intercept == false
+    ## Checking Gstack.resampling
+    @test Gstack.resampling isa CV
+    @test Gstack.resampling.nfolds == 2
+    ## Checking Gstack KNN models
+    @test Gstack.KNNClassifier_1.K == 3
+    ## Checking Gstack Logistic models
+    @test Gstack.LogisticClassifier_1.lambda == 1.0
+    @test Gstack.LogisticClassifier_1.fit_intercept == true
+    @test Gstack.LogisticClassifier_2.lambda == 1.0
+    @test Gstack.LogisticClassifier_2.fit_intercept == false
+end
+
+@testset "Continuous target TMLE built from configuration file" begin
+    tmle_config = joinpath("config", "continuous.toml")
+    tmle =  GenesInteraction.tmle_from_toml(TOML.parsefile(tmle_config))
+    @test tmle.fluctuation_family isa Normal
+
+    # Checking Qstack
+    Qstack = tmle.target_cond_expectation_estimator
+    ## Checking Qstack.metalearner
+    @test Qstack.metalearner isa LinearRegressor
+    @test Qstack.metalearner.fit_intercept == false
+    ## Checking Qstack.resampling
+    @test Qstack.resampling isa CV
+    @test Qstack.resampling.nfolds == 3
+    ## Checking Qstack KNN models
+    @test Qstack.KNNRegressor_1.K == 3
+    @test Qstack.KNNRegressor_1.leafsize == 20
+    @test Qstack.KNNRegressor_2.K == 5
+    @test Qstack.KNNRegressor_2.leafsize == 20
+    ## Checking Qstack Linear model
+    @test Qstack.LinearRegressor_1.fit_intercept == true
+
+    # Checking Gstack
+    Gstack = tmle.treatment_cond_likelihood_estimator
+    ## Checking Gstack.metalearner
+    @test Gstack.metalearner isa LogisticClassifier
+    @test Gstack.metalearner.fit_intercept == false
+    ## Checking Gstack.resampling
+    @test Gstack.resampling isa StratifiedCV
+    @test Gstack.resampling.nfolds == 2
+    ## Checking Gstack KNN models
+    @test Gstack.KNNClassifier_1.K == 3
+    ## Checking Gstack Logistic models
+    @test Gstack.LogisticClassifier_1.lambda == 1.0
+    @test Gstack.LogisticClassifier_1.fit_intercept == true
+    @test Gstack.LogisticClassifier_2.lambda == 1.0
+    @test Gstack.LogisticClassifier_2.fit_intercept == false
+end
+
+
+@testset "prepareTreatment" begin
+    snpqueries = CSV.File(snpfile) |> DataFrame
+    T = [2 0;
+        0 0;
+        2 3;
+        0 3]
+    T = GenesInteraction.prepareTreatment(T, first(snpqueries))
+    @test Tables.getcolumn(T, :x1) == [1, 0, 1, 0]
+    @test Tables.getcolumn(T, :x2) == [0, 0, 1, 1]
+end
+
+@testset "prepareTarget" begin
+    # categorical config
+    y = [true, false, true]
+    config = TOML.parsefile(joinpath("config", "categorical.toml"))
+    y = GenesInteraction.prepareTarget(y, config)
+    @test y == CategoricalArray{Bool,1,UInt32}([true, false, true])
+    # continuous config
+    y = [1, 0, 1]
+    config = TOML.parsefile(joinpath("config", "continuous.toml"))
+    @test GenesInteraction.prepareTarget(y, config) == y
+end
+
+@testset "Test filtering_idx" begin
+    snparray = [3 2 3 0;
+                0 2 0 1;
+                1 1 1 1;
+                3 0 0 1;
+                2 3 3 0;
+                0 0 2 2]
+    snpqueries = CSV.File(snpfile) |> DataFrame
+    snpquery = first(snpqueries)
+    snp_idx = Dict("rs3683945" => 2, "rs3706761"=> 1)
+    indices, columns = GenesInteraction.filtering_idx(snparray, snpquery, snp_idx)
+    @test columns == [2, 1]
+    @test indices == [1, 2, 4, 6]
+end
+
+@testset "Epistasis estimation run" begin
+
+    tmle_config = joinpath("config", "continuous.toml")
+    outfile = "tmleepistasis_results.csv"
+    tmleepistasis(genotypefile, 
+                    phenotypefile, 
+                    confoundersfile, 
+                    snpfile,
+                    tmle_config,
+                    outfile)
+    
+    try
+        results = CSV.File(outfile) |> DataFrame
+        @test results[!, "Estimate"] isa Vector
+        @test results[!, "Standard Error"] isa Vector
+    finally
+        rm(outfile)
+    end
+
+end
