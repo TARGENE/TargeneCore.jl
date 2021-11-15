a833b896b9d800cd92dfaef2ac225d56f0d80ead
Olivier Labayle <olabayle@gmail.com>
2021-11-15 18:17:16 +0100

    add XGBoost

--- src/models.jl
+++ src/models.jl
@@ -4,6 +4,7 @@
 SKLinearRegressor = @load LinearRegressor pkg=ScikitLearn verbosity=0
 KNNRegressor = @load KNNRegressor pkg=NearestNeighborModels verbosity=0
 LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity=0
+XGBoostRegressor = @load XGBoostRegressor pkg=XGBoost verbosity=0
 
 ###############################################################################
 # CLASSIFIERS
@@ -11,3 +12,4 @@ LinearRegressor = @load LinearRegressor pkg=MLJLinearModels verbosity=0
 SKLogisticClassifier = @load LogisticClassifier pkg=ScikitLearn verbosity=0
 LogisticClassifier = @load LogisticClassifier pkg=MLJLinearModels verbosity=0
 KNNClassifier = @load KNNClassifier pkg=NearestNeighborModels verbosity=0
+XGBoostClassifier = @load XGBoostClassifier pkg=XGBoost verbosity=0
