d5878f98b4b83feaedf858ffef8da7bda2ddd728
Olivier Labayle <olabayle@gmail.com>
2021-11-17 15:34:22 +0000

    update docs

--- ukbb_epistasis.jl
+++ ukbb_epistasis.jl
@@ -4,17 +4,18 @@ using GenesInteraction
 
 function parse_commandline()
     s = ArgParseSettings(
-        description = "This program computes estimates of Genes Epistatis from the UK-Biobank using TMLE."*
+        description = "This program computes estimates of Genetic Epistatis from the UK-Biobank using TMLE."*
                       "Here is a list of the arguments that should be provided, you can also have a look at the "*
                       "test/data and test/config folders to see some examples.",
         commands_are_required = false,
-        version = "0.1.0",
+        version = "0.1.6",
         add_version = true)
 
 
     @add_arg_table s begin
         "phenotypes"
-            help = "A file (.csv format). The first row contains the column names with `eid` the sample ID."
+            help = "A file (.csv format). The first row contains the column names with `eid` the sample ID"*
+                   " and the rest of the columns are phenotypes of interest."
             required = true
         "confounders"
             help = "A file (.csv format) containing the confounding variables values and the sample ids associated"*
@@ -25,8 +26,8 @@ function parse_commandline()
             help = "A file (.toml format) see: config/sample_query.toml for more information"
             required = true
         "estimator"
-            help = "A file (.toml format) describing the tmle estimator to use, see config/test_categorical.toml"*
-                   " or config/test_continuous.toml for a basic example."
+            help = "A file (.toml format) describing the tmle estimator to use, see config/sample_estimator.toml"*
+                   " for a basic example."
             required = true
         "output"
             help = "A path where the results will be saved (.csv format)"
