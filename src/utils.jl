66db9aa3b51cbfd957fcde9e2d1daa794233f8c6
Olivier Labayle <olabayle@gmail.com>
2022-01-24 15:18:03 +0000

    remove useless methods from utils

--- src/utils.jl
+++ src/utils.jl
@@ -1,16 +1,4 @@
 #####################################################################
-#####                 STACK COEFFS PARSING                       ####
-#####################################################################
-
-function Qstack_coefs(mach)
-    models = getfield(mach.model.Q̅, :models)
-    coeffs = [p[2] for p in fitted_params(mach).Q̅.metalearner.coefs]
-    return [m => c for (m,c) in zip(models, coeffs)]
-end
-
-repr_Qstack_coefs(mach; digits=2) = join([m => round(c, digits=digits) for (m, c) in Qstack_coefs(mach)], ", ")
-
-#####################################################################
 #####                     QUERY PARSING                          ####
 #####################################################################
 
@@ -54,84 +42,7 @@ function phenotypes_list(phenotype_listfile::String, done_phenotypes, allnames)
     filter(x -> (x ∈ Set(phenotypes_list.PHENOTYPES)) & (x ∉ done_phenotypes), allnames)
 end
 
-function init_or_retrieve_results(outfile, run_fn::typeof(PhenotypeTMLEEpistasis))
-    if isfile(outfile)
-        df = CSV.File(outfile, select=[:PHENOTYPE], types=Symbol) |> DataFrame
-    else
-        df = DataFrame(
-            PHENOTYPE=Symbol[],
-            QUERYNAME=String[],
-            QUERYSTRING=String[],
-            ESTIMATE=Float64[], 
-            PVALUE=Float64[],
-            LOWER_BOUND=Float64[],
-            UPPER_BOUND=Float64[],
-            STD_ERROR=Float64[],
-            QSTACK_COEFS=String[],
-            NROWS=Int[],
-            TCOUNTS=String[]
-            )
-        CSV.write(outfile, df)
-    end
-    return Set(df.PHENOTYPE)
-end
 
-function init_or_retrieve_results(outfile, run_fn::typeof(PhenotypeCrossValidation))
-    if isfile(outfile)
-        df = CSV.File(outfile, select=[:PHENOTYPE], types=Symbol) |> DataFrame
-    else
-        df = DataFrame(
-            PHENOTYPE=Symbol[],
-            VARIANTS=String[],
-            Q_RESULTSTRING=String[],
-            G_RESULTSTRING=String[],
-            )
-        CSV.write(outfile, df)
-    end
-    return Set(df.PHENOTYPE)
-end
-
-#####################################################################
-#####                TMLE REPORT PARSING                         ####
-#####################################################################
-
-get_query_report(reports::NamedTuple, i) = reports
-get_query_report(reports::Vector, i) = reports[i]
-
-
-#####################################################################
-#####                TREATMENT ENCODING                          ####
-#####################################################################
-
-function encode(T)
-    n, p = size(T)
-    if p == 1
-        return T[:, 1]
-    end
-
-    joint_levels_it = Iterators.product((levels(T[!, name]) for name in names(T))...)
-    encoding = Dict(Tuple(jl) => i for (i, jl) in enumerate(joint_levels_it))
-
-    t_target = Vector{Int}(undef, n)
-    for i in 1:n
-        t_target[i] = encoding[values(T[i, :])]
-    end
-
-    return categorical(t_target; levels=collect(values(encoding)))
-
-end
-
-#####################################################################
-#####                 TREATMENT COUNTS                           ####
-#####################################################################
-
-function TreatmentCountsRepr(T)
-    countrepr = join(names(T), " & ") * ": "
-    for row in eachrow(combine(groupby(T, names(T)), nrow))
-        countrepr *= join(row, " ") * " | "
-    end
-    return countrepr[1:end-3]
-end
 
 #####################################################################
 #####                 CV ADAPTIVE FOLDS                          ####
--- test/utils.jl
+++ test/utils.jl
@@ -13,31 +13,6 @@ using StableRNGs
 include("helper_fns.jl")
 
 
-@testset "Test retrieve coefficients" begin
-    estimatorfile = joinpath("config", "tmle_config.toml")
-    tmle_config = TOML.parsefile(estimatorfile)
-    queries = ["query_1" => (t₁=[1, 0], t₂=[1, 0]),]
-    tmle = TMLEEpistasis.estimators_from_toml(tmle_config, queries, TMLEEpistasis.PhenotypeTMLEEpistasis)
-
-    n = 1000
-    T = (t₁=categorical(rand([1, 0], n)), t₂=categorical(rand([1, 0], n)))
-    W = (w₁=rand(n),)
-    y = rand(n)
-    mach = machine(tmle["continuous"], T, W, y)
-    fit!(mach, verbosity=0)
-
-    coefs = TMLEEpistasis.Qstack_coefs(mach)
-    @test coefs isa Vector
-    @test length(coefs) == 5
-
-    repr = TMLEEpistasis.repr_Qstack_coefs(mach)
-    @test repr isa String
-    str_reprs = split(repr, ", ")
-    modelstr = join([split(v, " => ")[1] for v in str_reprs], ",")
-    @test modelstr == "HALRegressor,XGBoostRegressor,XGBoostRegressor,InteractionLMRegressor,ConstantRegressor"
-
-end
-
 @testset "Test parse_queries" begin
     build_query_file()
     queries = TMLEEpistasis.parse_queries(queryfile)
@@ -59,90 +34,15 @@ end
 
     @test TMLEEpistasis.phenotypes_list("data/phen_list_1.csv", [], allnames) == [:continuous_phenotype]
     @test TMLEEpistasis.phenotypes_list("data/phen_list_2.csv", [:continuous_phenotype], allnames) == [:categorical_phenotype]
-
-    # Test init_or_retrieve_results for PhenotypeTMLEEpistasis
-    outfile = "temp_results.csv"
-    @test TMLEEpistasis.init_or_retrieve_results(outfile, TMLEEpistasis.PhenotypeTMLEEpistasis) == Set()
-    init_df = CSV.File(outfile) |> DataFrame
-    @test names(init_df) == ["PHENOTYPE",
-                            "QUERYNAME",
-                            "QUERYSTRING",
-                            "ESTIMATE",
-                            "PVALUE",
-                            "LOWER_BOUND",
-                            "UPPER_BOUND",
-                            "STD_ERROR",
-                            "QSTACK_COEFS",
-                            "NROWS",
-                            "TCOUNTS"]
-
-    CSV.write(outfile, DataFrame(PHENOTYPE=["toto", "tata", "toto"]))
-    @test TMLEEpistasis.init_or_retrieve_results(outfile, TMLEEpistasis.PhenotypeTMLEEpistasis) == Set([:toto, :tata])
-    rm(outfile)
-
-    # Test init_or_retrieve_results for PhenotypeCrossValidation
-    @test TMLEEpistasis.init_or_retrieve_results(outfile, TMLEEpistasis.PhenotypeCrossValidation) == Set()
-    init_df = CSV.File(outfile) |> DataFrame
-    @test names(init_df) == ["PHENOTYPE",
-                            "VARIANTS",
-                            "Q_RESULTSTRING",
-                            "G_RESULTSTRING"]
-    CSV.write(outfile, DataFrame(PHENOTYPE=["toto", "tata", "toto"]))
-    @test TMLEEpistasis.init_or_retrieve_results(outfile, TMLEEpistasis.PhenotypeCrossValidation) == Set([:toto, :tata])
-    rm(outfile)
 end
 
-@testset "Test TMLE REPORT PARSING" begin
-    # Only one query, the reports is just a NamedTuple
-    reports = (pvalue=0.05,)
-    @test TMLEEpistasis.get_query_report(reports, 4) == reports
-    reports = [(pvalue=0.05,), (pvalue=0.01,)]
-    @test TMLEEpistasis.get_query_report(reports, 1) == reports[1]
-    @test TMLEEpistasis.get_query_report(reports, 2) == reports[2]
-
-end
-
-
-@testset "Test encode" begin
-    # Multiple treatments combined to joint treatment
-    T = DataFrame(
-        t₁=categorical(["AG", "GG", "AA", "AA", "AG"]),
-        t₂=categorical(["CT", "CC", "TT", "CT", "CT"]),
-        )
-
-    t_target = TMLEEpistasis.encode(T)
-    @test levels(t_target) == [1, 2, 9, 3, 5, 6, 8, 7, 4]
-    @test t_target == categorical([5, 3, 7, 4, 5])
-
-    # One treatment just returned
-    T = DataFrame(t₁=categorical(["AG", "GG", "AA", "AA", "AG"]))
-
-    t_target = TMLEEpistasis.encode(T)
-    @test t_target == ["AG", "GG", "AA", "AA", "AG"]
-end
-
-@testset "Test TreatmentCountsRepr" begin
-    T = DataFrame(
-        RS_100=categorical(["AA", "AG", "GG", "AA", "AG"]),
-        RS_10=categorical(["CC", "CG", "CG", "CC", "GG"])
-        )
-    
-    @test TMLEEpistasis.TreatmentCountsRepr(T) ==
-        "RS_100 & RS_10: AA CC 2 | AG CG 1 | AG GG 1 | GG CG 1"
-
-    T = DataFrame(
-        RS_100=categorical(["AA", "AG", "GG", "AA", "AG"]),
-    )
-    @test TMLEEpistasis.TreatmentCountsRepr(T) == "RS_100: AA 2 | AG 2 | GG 1"
-
-end
 
 @testset "Test set_cv_folds!" begin
     rng = StableRNG(123)
     tmle_config = joinpath("config", "tmle_config.toml")
     build_query_file()
     queries = TMLEEpistasis.parse_queries(queryfile)
-    tmles =  TMLEEpistasis.estimators_from_toml(TOML.parsefile(tmle_config), queries, TMLEEpistasis.PhenotypeTMLEEpistasis)
+    tmles =  TMLEEpistasis.estimators_from_toml(TOML.parsefile(tmle_config), queries)
 
     # Continuous y
     tmle = tmles["continuous"]
