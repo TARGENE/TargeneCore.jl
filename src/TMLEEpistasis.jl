4ad4424182b5caeb631d4d8771e15f7f482b1bcb
Olivier Labayle <olabayle@gmail.com>
2022-03-20 19:40:27 +0000

    migrate julia files from UKBBEpistasisPipeline

--- src/TMLEEpistasis.jl
+++ src/TMLEEpistasis.jl
@@ -1,5 +1,6 @@
 module TMLEEpistasis
 
+using MKL
 using DataFrames
 using MLJBase
 using CSV
@@ -12,6 +13,8 @@ using MLJModels
 using MLJLinearModels
 using Serialization
 using JLD2
+using SnpArrays
+using Mmap
 
 import MLJBase: fit, transform, target_scitype, input_scitype
 
@@ -23,10 +26,22 @@ include("ukbb.jl")
 include("models.jl")
 include("estimators.jl")
 include("utils.jl")
+include("asb_snps.jl")
+include("queries_generation.jl")
+include("confounders.jl")
+include("phenotypes.jl")
+include("grm.jl")
+include("sieve_plateau.jl")
 
 ###############################################################################
 # EXPORTS
 
 export UKBBVariantRun
+export generate_queries
+export filter_asb
+export filter_chromosome, merge_beds, adapt_flashpca
+export prepare_phenotypes, tmle_phenotypes_batches
+export grm_from_gcta
+export sieve_variance_plateau
 
 end
