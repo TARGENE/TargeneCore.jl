cf2dc019dd4580c2be8c7cb5490417c71b5983d2
Olivier Labayle <olabayle@gmail.com>
2022-03-31 17:10:39 +0100

    update to bullseye

--- build_sysimage.jl
+++ build_sysimage.jl
@@ -2,5 +2,6 @@ using PackageCompiler
 # Designed to be launched from the test directory
 create_sysimage(["TMLEEpistasis"]; 
                  sysimage_path="TMLEEpistasisSysimage.so",
-                 precompile_execution_file="example_script.jl")
+                 precompile_execution_file="example_script.jl",
+                 cpu_target=PackageCompiler.default_app_cpu_target())
 
