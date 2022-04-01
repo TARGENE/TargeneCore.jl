b0993c6f6cac510c01be6a452fa212300066095b
Olivier Labayle <olabayle@gmail.com>
2022-04-01 10:13:31 +0100

    try once again since it passed once else fallback to smaller image

--- example_script.jl
+++ example_script.jl
@@ -2,7 +2,5 @@ using TMLEEpistasis
 
 testdir = joinpath(pkgdir(TMLEEpistasis), "test")
 cd(testdir)
-include("models.jl")
-include("estimators.jl")
-include("utils.jl")
-include(joinpath(testdir, "ukbb.jl"))
+
+include(joinpath(testdir, "runtests.jl"))
