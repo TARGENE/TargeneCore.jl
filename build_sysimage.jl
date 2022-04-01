010bba6b3ccbc674219f810167dea2c06b3e1337
Olivier Labayle <olabayle@gmail.com>
2022-04-01 08:49:39 +0100

    try change cpu target

--- build_sysimage.jl
+++ build_sysimage.jl
@@ -2,6 +2,5 @@ using PackageCompiler
 # Designed to be launched from the test directory
 create_sysimage(["TMLEEpistasis"]; 
                  sysimage_path="TMLEEpistasisSysimage.so",
-                 precompile_execution_file="example_script.jl",
-                 cpu_target=PackageCompiler.default_app_cpu_target())
+                 precompile_execution_file="example_script.jl")
 
