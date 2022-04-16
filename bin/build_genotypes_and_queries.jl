1acabdd372a621c69a8f021ed24de786efca17f2
Olivier Labayle <olabayle@gmail.com>
2022-04-16 11:57:36 +0100

    add bin file for query and genotype generation

--- bin/build_genotypes_and_queries.jl
+++ bin/build_genotypes_and_queries.jl
@@ -8,20 +8,20 @@ function parse_commandline()
 
     @add_arg_table s begin
         "--mode", "-m"
-            help = "Specifies how the queries are generated:"*
-                   "The default,`frequency` will lead to a specification of the form "*
-                   "MAJOR:MAJOR -> MAJOR:MINOR for each SNP"
+            help = "`given` or `asb`. If `given` then the option `--query-prefix` must be"*
+                   " provided. If `asb` options `--asb-prefix` and `--trans-actors must be`"*
+                   " provided. Queries will be generated as MAJOR:MAJOR -> MAJOR:MINOR for each SNP."
             arg_type = String
-            default = "frequency"
+            default = "given"
         "--outdir", "-o"
             help = "Out directory where the queries will be saved"
             arg_type = String
             default = "."
         "--query-prefix", "-q"
-            help = "Prefix path to queries"
+            help = "Prefix path to queries, must be provided if mode is: `given`"
             arg_type = String
-            
-        "--calling-threshold", "-t"
+            required = false
+        "--call-threshold", "-c"
             arg_type = Float64
             help = "This is written down on the query file, it is the threshold that"*
                     " is later used to hard call genotypes based on their probabilities"
@@ -35,15 +35,15 @@ function parse_commandline()
             help = "Minor genotype frequency filter"
             default = 0.001
             required = false
-        "asb-prefix"
+        "--asb-prefix", "-a"
             arg_type = String
             help = "Prefix path to filtered allelic-specific binding SNPS output by baal-nf "*
                    "see https://git.ecdf.ed.ac.uk/oalmelid/baal-nf"
-            required = true
-        "trans-actors"
+            required = false
+        "--trans-actors", "-t"
             arg_type = String
             help = "Path to trans-acting SNPS"
-            required = true
+            required = false
         "chr-prefix"
             help = "Prefix path to BGEN chromosomes from the UKBB."
             arg_type = String
@@ -57,4 +57,4 @@ end
 parsed_args = parse_commandline()
 
 
-generate_queries(parsed_args)
+build_genotypes_and_queries(parsed_args)
