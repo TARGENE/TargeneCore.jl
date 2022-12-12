67df6f9df6daf5b9f3db644c43210655037a0978
Olivier Labayle <olabayle@gmail.com>
2022-12-12 15:45:48 +0000

    cleanup repo

--- src/TargeneCore.jl
+++ src/TargeneCore.jl
@@ -1,16 +1,10 @@
 module TargeneCore
 
-if occursin("Intel", Sys.cpu_info()[1].model)
-    using MKL
-end
 using DataFrames
 using CSV
-using TMLE
 using BGEN
-using JLD2
 using SnpArrays
 using Mmap
-using HypothesisTests
 using YAML
 using Combinatorics
 
@@ -18,7 +12,6 @@ using Combinatorics
 # INCLUDES
 
 include("confounders.jl")
-include("sieve_plateau.jl")
 include(joinpath("tmle_inputs", "tmle_inputs.jl"))
 include(joinpath("tmle_inputs", "from_actors.jl"))
 include(joinpath("tmle_inputs", "from_param_files.jl"))
@@ -28,7 +21,6 @@ include(joinpath("tmle_inputs", "from_param_files.jl"))
 # EXPORTS
 
 export filter_chromosome, merge_beds, adapt_flashpca
-export sieve_variance_plateau
 export tmle_inputs
 
 end
