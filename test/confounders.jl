5ebe3b198dd646a714ebe48ef9edb0434daab877
s2223108 <b.j.roskams-hieter@sms.ed.ac.uk>
2023-11-16 09:25:48 +0000

    add comment back in

--- test/confounders.jl
+++ test/confounders.jl
@@ -56,6 +56,7 @@ end
     
     clean(parsed_args)
 
+    # No QC file provided
     parsed_args = Dict(
         "input"  => SnpArrays.datadir("mouse"),
         "output" => joinpath("data", "filtered-mouse"),
